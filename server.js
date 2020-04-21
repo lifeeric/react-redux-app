38b9b3ea53c194508b4ccf3af6396de6a2f82485
LIFE <dkrshw65@gmail.com>
2020-04-21 04:07:24 -0400

    deploy 1

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path    = require('path');
 
 const app = express();
 
-app.use(express.static('./build');
+app.use(express.static(path.join(__dirname, 'build'));
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 const PORT = process.env.PORT || 8000;
